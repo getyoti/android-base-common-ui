5e40866e88ac278f20177afae012d21642d74e95
Cyrille Quemin <cyrille.quemin@gmail.com>
2018-05-04 16:20:20 +0100

    Added: changes for 0.0.5. Fixed padding issue on YotiButton Added common resources

---
+++ commons-ui/src/main/java/com/yoti/mobile/android/commons/ui/widget/YotiButton.java
@@ -0,0 +1,22 @@
+package com.yoti.mobile.android.commons.ui.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.view.ContextThemeWrapper;
+import com.yoti.mobile.android.commons.ui.R;
+
+public class YotiButton extends AppCompatButton {
+
+    public YotiButton(Context context) {
+        this(context, null, 0);
+    }
+
+    public YotiButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public YotiButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(new ContextThemeWrapper(context, R.style.Yoti_Widget_Button), attrs, defStyleAttr);
+    }
+}
--- sample/src/main/java/com/yoti/mobile/android/commons/ui/sample/MainActivity.java
+++ sample/src/main/java/com/yoti/mobile/android/commons/ui/sample/MainActivity.java
@@ -1,17 +1,25 @@
 package com.yoti.mobile.android.commons.ui.sample;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    View progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.yoti_progress_bar);
 
         findViewById(R.id.yoti_button).setOnClickListener(new OnClickListener() {
             @Override
@@ -19,5 +27,35 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        findViewById(R.id.progress_bar_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 1500);
+            }
+        });
+
+        ((CheckBox) findViewById(R.id.update_btn_states_checkbox)).setOnCheckedChangeListener(
+            new OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    updateButtonsState(!isChecked);
+                }
+            });
+    }
+
+    private void updateButtonsState(boolean enabled) {
+        findViewById(R.id.yoti_button).setEnabled(enabled);
+        findViewById(R.id.blue_button).setEnabled(enabled);
+        findViewById(R.id.green_button).setEnabled(enabled);
+        findViewById(R.id.white_button).setEnabled(enabled);
+        findViewById(R.id.progress_bar_button).setEnabled(enabled);
     }
 }
